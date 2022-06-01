fccb97f06fcf00aa4e342f274acd5f1c0abf0a87
Fernando Velasco Salazar <fersalazar@ip-172-27-0-174.ec2.internal>
2022-06-01 13:19:01 -0500

    Cleaned the boilerplate comments Fix the authentication problem by adding basic auth to getAccessToken Fix the authentication problem by adding basic auth to refreshAccessToken Return the display_name in the testAuth function

--- authentication.js
+++ authentication.js
@@ -2,9 +2,8 @@
 
 const getAccessToken = async (z, bundle) => {
   const concatedatedString = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
-  z.console.log('concatenated', concatedatedString);
   const header = `Basic ${Buffer.from(concatedatedString).toString('base64')}`;
-  z.console.log('header ', header);
+
   const response = await z.request({
     url: 'https://www.reddit.com/api/v1/access_token',
     method: 'POST',
@@ -15,16 +14,10 @@ const getAccessToken = async (z, bundle) => {
     },
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
-      'Authorization': `Basic ${header}`,
+      Authorization: header,
     },
   });
 
-  // If you're using core v9.x or older, you should call response.throwForStatus()
-  // or verify response.status === 200 before you continue.
-
-  // This function should return `access_token`.
-  // If your app does an app refresh, then `refresh_token` should be returned here
-  // as well
   return {
     access_token: response.data.access_token,
     refresh_token: response.data.refresh_token,
@@ -33,9 +26,7 @@ const getAccessToken = async (z, bundle) => {
 
 const refreshAccessToken = async (z, bundle) => {
   const concatedatedString = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
-  z.console.log('concatenated', concatedatedString);
   const header = `Basic ${Buffer.from(concatedatedString).toString('base64')}`;
-  z.console.log('header ', header);
   const response = await z.request({
     url: 'https://www.reddit.com/api/v1/access_token',
     method: 'POST',
@@ -45,25 +36,17 @@ const refreshAccessToken = async (z, bundle) => {
     },
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
-      'Authorization': `Basic ${header}`,
+      Authorization: header,
     },
   });
 
-  // If you're using core v9.x or older, you should call response.throwForStatus()
-  // or verify response.status === 200 before you continue.
-
-  // This function should return `access_token`.
-  // If the refresh token stays constant, no need to return it.
-  // If the refresh token does change, return it here to update the stored value in
-  // Zapier
   return {
     access_token: response.data.access_token,
     refresh_token: response.data.refresh_token,
   };
 };
 
-// This function runs before every outbound request. You can have as many as you
-// need. They'll need to each be registered in your index.js file.
+
 const includeBearerToken = (request, z, bundle) => {
   if (bundle.authData.access_token) {
     request.headers.Authorization = `Bearer ${bundle.authData.access_token}`;
@@ -72,18 +55,16 @@ const includeBearerToken = (request, z, bundle) => {
   return request;
 };
 
-// You want to make a request to an endpoint that is either specifically designed
-// to test auth, or one that every user will have access to. eg: `/me`.
-// By returning the entire request object, you have access to the request and
-// response data for testing purposes. Your connection label can access any data
-// from the returned response using the `json.` prefix. eg: `{{json.username}}`.
-const test = (z, bundle) =>
-  z.request({ url: 'https://oauth.reddit.com/api/v1/me' });
+const testAuth = async(z, bundle) => {
+  const meResponse = await z.request({ url: 'https://oauth.reddit.com/api/v1/me' });
+  meResponse.throwForStatus();
+
+  return meResponse.display_name;
+};
+  
 
 module.exports = {
   config: {
-    // OAuth2 is a web authentication standard. There are a lot of configuration
-    // options that will fit most any situation.
     type: 'oauth2',
     oauth2Config: {
       authorizeUrl: {
@@ -93,7 +74,7 @@ module.exports = {
           state: '{{bundle.inputData.state}}',
           redirect_uri: '{{bundle.inputData.redirect_uri}}',
           response_type: 'code',
-          scope: 'identity',
+          scope: 'identity submit save read',
           duration: 'permanent',
         },
       },
@@ -101,23 +82,9 @@ module.exports = {
       refreshAccessToken,
       autoRefresh: true,
     },
-
-    // Define any input app's auth requires here. The user will be prompted to enter
-    // this info when they connect their account.
     fields: [],
-
-    // The test method allows Zapier to verify that the credentials a user provides
-    // are valid. We'll execute this method whenever a user connects their account for
-    // the first time.
-    test,
-
-    // This template string can access all the data returned from the auth test. If
-    // you return the test object, you'll access the returned data with a label like
-    // `{{json.X}}`. If you return `response.data` from your test, then your label can
-    // be `{{X}}`. This can also be a function that returns a label. That function has
-    // the standard args `(z, bundle)` and data returned from the test can be accessed
-    // in `bundle.inputData.X`.
-    connectionLabel: '{{json.username}}',
+    test: testAuth,
+    connectionLabel: testAuth,
   },
   befores: [includeBearerToken],
   afters: [],
